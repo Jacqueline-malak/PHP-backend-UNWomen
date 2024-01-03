eac7fc804fd79712f431eba83e4af71f7067479d
Jacqueline-malak <108737368+Jacqueline-malak@users.noreply.github.com>
2024-01-03 15:01:35 +0200

    Add files via upload
    Signed-off-by: Jacqueline-malak <108737368+Jacqueline-malak@users.noreply.github.com>

---
+++ Day 3 PHP backend.php
@@ -0,0 +1,34 @@
+<?php
+$First_name='Jacqueline Abdel_Malak';
+$Age='40';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Welcome to my First PHP code</h1>
+<p>PHP is Fun!<br>
+"Hello world!<br>
+"I'm about to learn PHP!<br>
+</p>
+<b>Hello</b>
+<em>
+<b>
+<?php echo $First_name
+?>
+</em><b>
+
+<br>
+<p>Age:
+    <?php 
+    echo "$Age years old";
+    ?>
+    </p>    
+</body>
+</html>

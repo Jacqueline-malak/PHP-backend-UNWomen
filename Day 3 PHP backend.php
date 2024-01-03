bc4ab2cc74e599c045d4e1a038d791fd726f3a54
Jacqueline-malak <108737368+Jacqueline-malak@users.noreply.github.com>
2024-01-03 13:51:38 +0200

    Add files via upload
    Signed-off-by: Jacqueline-malak <108737368+Jacqueline-malak@users.noreply.github.com>

---
+++ Day 3 PHP backend.php
@@ -0,0 +1,11 @@
+<?php
+$First_name='Jacqueline';
+$Last_name='Abdel_Malak';
+$Age='40';
+echo "Welcome $First_name";
+echo "<br>";
+echo $Last_name;
+echo "<br>";
+echo "Iam $Age years old";
+echo "<br>";
+?>
